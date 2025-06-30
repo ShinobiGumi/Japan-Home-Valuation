96507c316e56c6b35e0568877ae685323fb87b87
ShinobiGumi <carlokreuger@protonmail.com>
2025-06-30 10:59:13 +0800

    SEO1

--- pages/about.tsx
+++ pages/about.tsx
@@ -4,7 +4,7 @@ import Layout from "../components/Layout";
 export default function About() {
   return (
     <Layout>
-      <Head><Head>
+      <Head>
         <title>About Japan Home Advisor | Independent Property Advice in Japan</title>
         <meta
             name="description"
